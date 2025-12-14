d6699aea5254e556b25301236470906b03580bb8
Nelson <xavier.nelson@codework.ai>
2025-12-14 15:06:41 +0530

    commit

--- app/ai-solutions/page.js
+++ app/ai-solutions/page.js
@@ -1,14 +1,13 @@
 import React from 'react'
 import AiLandingPage from '../components/aiSolutions/aiLandingPage'
 import AiCardSection from '../components/aiSolutions/aiCardSection'
-import DrivingInnovation from '../components/aiSolutions/drivingInnovation'
 
 const page = () => {
   return (
     <div>
       <AiLandingPage/>
       <AiCardSection/>
-      <DrivingInnovation/> 
+      {/*<DrivingInnovation/> }*/}
       {/* <FooterHeading/>  */}
     </div>
   )
